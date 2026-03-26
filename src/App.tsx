75c87c3539a69fb4898418acb9345dbce14cfbc8
bryanhce <ho.cheng.en.bryan@gmail.com>
2026-03-26 12:16:16 +0800

    add full screen scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import { DiceProvider } from "./context/DiceContext";
 export default function App() {
     return (
         <DiceProvider>
-            <div className="flex flex-col md:flex-row h-screen w-screen overflow-hidden font-sans">
+            <div className="flex flex-col md:flex-row min-h-screen w-screen font-sans">
                 {/* 70% Left Panel */}
-                <div className="w-full h-[60%] md:w-[70%] md:h-full">
+                <div className="w-full h-[60vh] md:w-[70%] md:h-screen">
                     <DiceArena />
                 </div>
 
                 {/* 30% Right Panel */}
-                <div className="w-full h-[40%] md:w-[30%] md:h-full">
+                <div className="w-full md:w-[30%]">
                     <InfoPanel />
                 </div>
             </div>
